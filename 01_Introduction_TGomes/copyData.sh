48b1dc2c71ab2346e9e9777999cc3c9ab64f546a
Tomás Gomes <tomasgomes@users.noreply.github.com>
2026-05-20 08:10:51 +0100

    Added tutorials

---
+++ 01_Introduction_TGomes/copyData.sh
@@ -0,0 +1,5 @@
+# copy the R code into your home folder
+cp /mnt/nfs/EMBO/IntroAnalysis/AnalysisIntro.Rmd ~
+
+# copy the Python code into your home folder
+cp /mnt/nfs/EMBO/IntroAnalysis/scanpy_tutorial.ipynb ~
